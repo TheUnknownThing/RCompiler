d968df99f2c1864e0e50488f037788542e445c3d
TheUnknownThing <wanghn051@outlook.com>
2025-08-05 23:43:08 +0800

    feat: update preprocessor test to include standard output verification

--- tests/unit/preprocessor.cpp
+++ tests/unit/preprocessor.cpp
@@ -1,12 +1,39 @@
 #include "../../include/preprocessor/preprocessor.hpp"
+#include <assert.h>
 
 int main() {
   rc::Preprocessor p("../../examples/comments.rs");
   auto processed_lines = p.preprocess();
 
-  for (const std::string &line : processed_lines) {
-    std::cout << line << std::endl;
+  auto standard_output = R"(pub mod nested_comments {
+let a = "/* aaaaaaaaaaaaa */";
+let b = '/';
+let c = "/*";
+let d = "*/";
+let e = "/* not a /* nested */ comment */";
+let f = "///";
+let g = "\\\" /* this is not a comment */
+\"";
+let h = "\\";
+let i = 12 13;
+let j = 12
+;
+}
+pub mod outer_module {
+pub mod inner_module {}
+pub mod degenerate_cases {
+pub mod dummy_item {}
+}
+}
+)";
+
+  std::string output;
+  for (const auto &line : processed_lines) {
+    output += line + "\n";
   }
 
+  assert(output == standard_output);
+
+  std::cout << "Test passed!" << std::endl;
   return 0;
 }
